01b5295425edab10463a85b90ef85516426de129
Igor Shmel <shmel@system32.ru>
2020-02-07 15:22:37 +0300

    Update rsalib.go

--- rsalib.go
+++ rsalib.go
@@ -1,4 +1,4 @@
-package rsa
+package rsatest
 
 import (
 	"bufio"
@@ -7,57 +7,57 @@ import (
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/pem"
 	"os"
 )
+
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // PKCS1 Sign create
 /////////////////////////////////////////////////////////////////////////////////////////////////
-func SignRsaPKCS1 (Priv *rsa.PrivateKey, digest string) ([]byte, error) {
+func SignRsaPKCS1(Priv *rsa.PrivateKey, digest string) ([]byte, error) {
 
-	byteMessage := []byte(digest)
-	pssMessage := byteMessage
+	message := []byte(digest)
 	newHash := crypto.SHA256
 	pssNewHash := newHash.New()
 
-	if _, err := pssNewHash.Write(pssMessage); err != nil {
-		return  []byte(""),err
+	if _, err := pssNewHash.Write(message); err != nil {
+		return []byte(""), err
 	}
 	hashed := pssNewHash.Sum(nil)
 
 	signature, err := rsa.SignPKCS1v15(rand.Reader, Priv, newHash, hashed)
 	if err != nil {
-		return  []byte(""),err
+		return []byte(""), err
 	}
 
-	return  signature, nil
+	return signature, nil
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // PKCS1 Verify sign
 /////////////////////////////////////////////////////////////////////////////////////////////////
-func VerifyRsaPKCS1 (Pub *rsa.PublicKey, byteSig []byte, digest string) (bool, error) {
+func VerifyRsaPKCS1(Pub *rsa.PublicKey, byteSig []byte, digest string) (bool, error) {
 
-	byteMessage := []byte(digest)
-	pssMessage := byteMessage
+	message := []byte(digest)
 	newHash := crypto.SHA256
 	pssNewHash := newHash.New()
 
-	if _, err := pssNewHash.Write(pssMessage); err != nil {
-		return  false, err
+	if _, err := pssNewHash.Write(message); err != nil {
+		return false, err
 	}
 	hashed := pssNewHash.Sum(nil)
 
 	//Verify Signature
 	if err := rsa.VerifyPKCS1v15(Pub, newHash, hashed, byteSig); err != nil {
-		return  false, err
+		return false, err
 	}
 
-	return  true, nil
+	return true, nil
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // PSS Sign create
 /////////////////////////////////////////////////////////////////////////////////////////////////
-func SignRsaPss (Priv *rsa.PrivateKey, digest string) ([]byte, error) {
+func SignRsaPss(Priv *rsa.PrivateKey, digest string) ([]byte, error) {
 
 	var opts rsa.PSSOptions
 
@@ -68,21 +68,21 @@ func SignRsaPss (Priv *rsa.PrivateKey, digest string) ([]byte, error) {
 	pssNewHash := newHash.New()
 
 	if _, err := pssNewHash.Write(pssMessage); err != nil {
-		return  []byte(""),err
+		return []byte(""), err
 	}
 	hashed := pssNewHash.Sum(nil)
 
 	signature, err := rsa.SignPSS(rand.Reader, Priv, newHash, hashed, &opts)
 	if err != nil {
-		return  []byte(""),err
+		return []byte(""), err
 	}
 
-	return  signature, nil
+	return signature, nil
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // PSS Verify sign
 /////////////////////////////////////////////////////////////////////////////////////////////////
-func VerifyRsaPss (Pub *rsa.PublicKey, byteSig []byte, digest string) (bool, error) {
+func VerifyRsaPss(Pub *rsa.PublicKey, byteSig []byte, digest string) (bool, error) {
 
 	var opts rsa.PSSOptions
 
@@ -93,16 +93,16 @@ func VerifyRsaPss (Pub *rsa.PublicKey, byteSig []byte, digest string) (bool, err
 	pssNewHash := newHash.New()
 
 	if _, err := pssNewHash.Write(pssMessage); err != nil {
-		return  false, err
+		return false, err
 	}
 	hashed := pssNewHash.Sum(nil)
 
 	//Verify Signature
 	if err := rsa.VerifyPSS(Pub, newHash, hashed, byteSig, &opts); err != nil {
-		return  false, err
+		return false, err
 	}
 
-	return  true, nil
+	return true, nil
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // RSA encrypt digest
@@ -116,13 +116,13 @@ func EncryptRSA(Pub *rsa.PublicKey, dgst string) ([]byte, error) {
 	if encryptText, err := rsa.EncryptOAEP(hash, rand.Reader, Pub, message, label); err == nil {
 		return encryptText, err
 	} else {
-		return []byte(""),err
+		return []byte(""), err
 	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // RSA decript digest
 /////////////////////////////////////////////////////////////////////////////////////////////////
-func DecriptRSA(Priv *rsa.PrivateKey, message []byte)  ([]byte, error)  {
+func DecriptRSA(Priv *rsa.PrivateKey, message []byte) ([]byte, error) {
 
 	label := []byte("")
 	hash := sha256.New()
@@ -130,7 +130,7 @@ func DecriptRSA(Priv *rsa.PrivateKey, message []byte)  ([]byte, error)  {
 	if decriptText, err := rsa.DecryptOAEP(hash, rand.Reader, Priv, message, label); err == nil {
 		return decriptText, err
 	} else {
-		return []byte(""),err
+		return []byte(""), err
 	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -138,19 +138,24 @@ func DecriptRSA(Priv *rsa.PrivateKey, message []byte)  ([]byte, error)  {
 /////////////////////////////////////////////////////////////////////////////////////////////////
 func GeneratePairKeys(path string, name string) error {
 
-	var err error
 	// Generate RSA Keys
 	if PrivateKey, err := rsa.GenerateKey(rand.Reader, 2048); err == nil {
 		PublicKey := &PrivateKey.PublicKey
 
 		if err = SavePrivateKey(path + name + "PrivateKey.pem", PrivateKey); err == nil {
-			err = SavePublicKey(path + name + "PublicKey.pub", PublicKey)
+			if err = SavePublicKey(path + name + "PublicKey.pub", PublicKey); err != nil {
+				return err
+			}
+		} else {
+			return  err
 		}
+	} else {
+		return  err
 	}
-	return err
+	return nil
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
-// load Private Key from file
+// save Public Key
 /////////////////////////////////////////////////////////////////////////////////////////////////
 func SavePublicKey(fName string, pubkey *rsa.PublicKey) error {
 
@@ -167,17 +172,17 @@ func SavePublicKey(fName string, pubkey *rsa.PublicKey) error {
 		}
 		err = pem.Encode(pemfile, pemkey)
 	}
-	return  err
+	return err
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
-// save Private Key from file
+// save Private Key
 /////////////////////////////////////////////////////////////////////////////////////////////////
 func SavePrivateKey(fName string, key *rsa.PrivateKey) error {
 
 	var err error
 	if outFile, err := os.Create(fName); err == nil {
 		defer func() {
-			 err = outFile.Close()
+			err = outFile.Close()
 		}()
 
 		//converts a private key to ASN.1 DER encoded form.
@@ -187,59 +192,104 @@ func SavePrivateKey(fName string, key *rsa.PrivateKey) error {
 		}
 		err = pem.Encode(outFile, privateKey)
 	}
-	return  err
+	return err
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // load Private Key from file
 /////////////////////////////////////////////////////////////////////////////////////////////////
-func LoadPrivateKey(fileName string)   (*rsa.PrivateKey, error)  {
+func LoadPrivateKeyFromFile(fileName string) (*rsa.PrivateKey, error) {
 
-	var err error
 	if privateKeyFile, err := os.Open(fileName); err == nil {
 		if pemFileInfo, err := privateKeyFile.Stat(); err == nil {
 
-			size  := pemFileInfo.Size()
+			size := pemFileInfo.Size()
 			pemBytes := make([]byte, size)
 			buffer := bufio.NewReader(privateKeyFile)
 
 			if _, err = buffer.Read(pemBytes); err == nil {
-				data,_ := pem.Decode([]byte(pemBytes))
+				data, _ := pem.Decode([]byte(pemBytes))
 				if err = privateKeyFile.Close(); err == nil {
 					if privateKeyFileImported, err := x509.ParsePKCS1PrivateKey(data.Bytes); err == nil {
-
 						return privateKeyFileImported, nil
+					} else {
+						return nil, err
 					}
+				} else {
+					return nil, err
 				}
+			} else {
+				return nil, err
 			}
+		} else {
+			return nil, err
 		}
+	} else {
+		return nil, err
 	}
-	return nil, err
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // load Public Key from file
 /////////////////////////////////////////////////////////////////////////////////////////////////
-func LoadPublicKey(fileName string)  (*rsa.PublicKey, error) {
+func LoadPublicKeyFromFile(fileName string) (*rsa.PublicKey, error) {
 
-	var err error
 	if publicKeyFile, err := os.Open(fileName); err == nil {
 		if pemFileInfo, err := publicKeyFile.Stat(); err == nil {
 
-			size  := pemFileInfo.Size()
+			size := pemFileInfo.Size()
 			pemBytes := make([]byte, size)
 			buffer := bufio.NewReader(publicKeyFile)
 
 			if _, err = buffer.Read(pemBytes); err == nil {
-				data,_ := pem.Decode([]byte(pemBytes))
+				data, _ := pem.Decode([]byte(pemBytes))
 				if err = publicKeyFile.Close(); err == nil {
 					if publicKeyFileImported, err := x509.ParsePKCS1PublicKey(data.Bytes); err == nil {
 
 						return publicKeyFileImported, nil
+					} else {
+						return nil, err
 					}
+				} else {
+					return nil, err
 				}
+			} else {
+				return nil, err
 			}
+		} else {
+			return nil, err
 		}
+	} else {
+		return nil, err
 	}
-	return nil, err
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// load Public Key from Base64 string
+/////////////////////////////////////////////////////////////////////////////////////////////////
+func LoadPublicKey(strPubBase64 string) (*rsa.PublicKey, error) {
+
+	if bytePub, err := base64.StdEncoding.DecodeString(strPubBase64); err == nil {
+		if publicKey, err := x509.ParsePKCS1PublicKey([]byte(bytePub)); err == nil {
+			return publicKey, nil
+		} else {
+			return nil, err
+		}
+	} else {
+		return nil, err
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////
+// load Private Key from Base64 string
+/////////////////////////////////////////////////////////////////////////////////////////////////
+func LoadPrivateKey(strPrivBase64 string) (*rsa.PrivateKey, error) {
+
+	if bytePriv, err := base64.StdEncoding.DecodeString(strPrivBase64); err == nil {
+		if privateKeyFileImported, err := x509.ParsePKCS1PrivateKey(bytePriv); err == nil {
+			return privateKeyFileImported, nil
+		} else {
+			return nil, err
+		}
+	} else {
+		return nil, err
+	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // is File Exists ?
@@ -251,4 +301,5 @@ func FileExists(filename string) bool {
 		return false
 	}
 	return !info.IsDir()
-} 
+}
+
